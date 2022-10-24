cbe215d4b0b5512547e1a02c997abf33ecfd8908
root <vmshay@tomtit.tomsk.ru>
2022-10-24 07:21:30 -0400

    добавлен файл с SQL запросами

--- Moodle/send_calendar.py
+++ Moodle/send_calendar.py
@@ -2,16 +2,20 @@
 #
 import paramiko
 import bot.config as cnf
+import shutil
 
 file = 'calendar.ics'
+folder = '/var/www/vmshay/calendar.ics'
 
 
 async def send():
-    with paramiko.SSHClient() as ssh:
-        ssh.load_system_host_keys()
-        ssh.connect(hostname=cnf.SSH_HOST, username=cnf.SSH_USER, password=cnf.SSH_PASS)
+    shutil.copy(file, folder)
+    # with paramiko.SSHClient() as ssh:
+    #     ssh.load_system_host_keys()
+    #     ssh.connect(hostname=cnf.SSH_HOST, username=cnf.SSH_USER, password=cnf.SSH_PASS)
+    #
+    #     sftp = ssh.open_sftp()
+    #
+    #     sftp.chdir('/var/www/vmshay')
+    #     sftp.put(file, file)
 
-        sftp = ssh.open_sftp()
-
-        sftp.chdir('/var/www/vmshay')
-        sftp.put(file, file)
---
+++ bot/sql.py
@@ -0,0 +1,69 @@
+
+
+def check_admin(data):
+    sql = f"select admin from user_table where tg_id = {data}"
+    return sql
+
+
+def check_id(data):
+    sql = f'select tg_id from user_table where tg_id ={data}'
+    return sql
+
+
+def check_approved(data):
+    sql = f'select approved from user_table where tg_id ={data}'
+    return sql
+
+
+def get_user_event(data):
+    sql = f"select events_table.description, user_table.name, events_table.e_date, events_table.e_start, events_table.e_end" \
+        f" from events_table" \
+        f" inner join user_table on events_table.owner = user_table.tg_id where events_table.owner={data}"
+    return sql
+
+
+def get_all_events(data):
+    sql = f"select events_table.description, user_table.name, events_table.e_date, events_table.e_start, events_table.e_end from events_table" \
+          f" inner join user_table on events_table.owner = user_table.tg_id where events_table.e_date={data}"
+
+    return sql
+
+
+def get_range_events(data):
+    sql = f"select events_table.description, user_table.name, events_table.e_date, events_table.e_start, events_table.e_end " \
+                                   f"from events_table inner join user_table " \
+                                   f"on events_table.owner = user_table.tg_id " \
+                                   f"where events_table.e_date between {data}"
+    return sql
+
+
+def sql_send(data):
+    sql = f"INSERT INTO user_table (tg_id,name,phone) VALUES ({data['id']},'{data['FIO']}',{data['number']})"
+    return sql
+
+
+def sql_booked(data):
+    sql = f"select events_table.id, user_table.name, user_table.phone, events_table.description, events_table.e_start, events_table.e_end from events_table inner join user_table on events_table.owner = user_table.tg_id WHERE events_table.e_date = '{data}'"
+    return sql
+
+
+def sql_booked_time(data):
+    sql = f"select e_start ,e_end from events_table where e_date = '{data}'"
+    return sql
+
+
+def sql_send_event(data):
+    sql = f"insert into events_table (e_date, owner, description, e_start, e_end) values ({data['date']},{data['owner']},'{data['description']}','{data['t_start']}','{data['t_end']}')"
+    return sql
+
+
+def sql_manage_events():
+    sql = f"select events_table.id, user_table.name, user_table.phone, events_table.description, events_table.e_date, events_table.e_start, events_table.e_end from events_table inner join user_table on events_table.owner = user_table.tg_id WHERE events_table.approved = 0"
+    return sql
+
+
+def sql_all_events():
+    sql = f"select events_table.id, events_table.description, user_table.name, events_table.e_date, events_table.e_start, events_table.e_end" \
+          f" from events_table" \
+          f" inner join user_table on events_table.owner = user_table.tg_id where events_table.approved = '1'"
+    return sql
